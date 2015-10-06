8953d5b9552133993af009305c111b2ec083e5f1
Yann Petrick <yann.petrick@meetrics.de>
2015-10-06 09:10:08 +0200

    inital commit

---
+++ src/main/java/xyz/eval/main/Main.java
@@ -0,0 +1,14 @@
+package xyz.eval.main;
+
+/**
+ *
+ * @author Y. Petrick
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        // TODO Auto-generated method stub
+
+    }
+
+}
